cc3a09606882813b828650fcbf370a7776eaca00
Victor Guzman <fawques7@hotmail.com>
2012-07-18 20:32:01 +0200

    refiring deshecho, ahora loguea los mouseover

--- js/refiring.js
+++ js/refiring.js
@@ -1,29 +1,29 @@
 /* Probando a relanzar los eventos de ratón desde SplitCellBackground hacia el trigger del bubbletip */
 
 function refiring(){
-	$('.bubbleInfo').bind('mouseenter',function(e){
-		$('.trigger',this).mouseenter();
+	$('.bubbleInfo').bind('mouseenter',function (e){
+		console.log("Ratón entra en: " + this + ", e = " + e);
 		e.stopPropagation();
 	});
-	$('.SplitCellBackground').bind('mouseenter',function(e){
-		$('.bubbleInfo',this).mouseenter();
+	$('.SplitCellBackground').bind('mouseenter',function (e){
+		console.log("Ratón entra en: " + this+ ", e = " + e);
 		e.stopPropagation();
 	});
-	$('.holiday').bind('mouseenter',function(e){
-		$('.SplitCellBackground',this).mouseenter();
+	$('.holiday').bind('mouseenter',function (e){
+		console.log("Ratón entra en: " + this+ ", e = " + e);
 		e.stopPropagation();
 	});
 	
 	$('.bubbleInfo').bind('mouseleave',function(e){
-		$('.trigger',this).mouseleave();
+		console.log("Ratón sale de: " + this+ ", e = " + e);
 		e.stopPropagation();
 	});
 	$('.SplitCellBackground').bind('mouseleave',function(e){
-		$('.bubbleInfo',this).mouseleave();
+		console.log("Ratón sale de: " + this+ ", e = " + e);
 		e.stopPropagation();
 	});
 	$('.holiday').bind('mouseleave',function(e){
-		$('.SplitCellBackground',this).mouseleave();
+		console.log("Ratón sale de: " + this+ ", e = " + e);
 		e.stopPropagation();
 	});
 }
